036ea8c39c0063a0c330cfd1242d114b4a920fa1
Alex Orlet <AlexOrlet89@gmail.com>
2022-03-14 18:22:36 -0700

    useEffect in Main function returns main that map loops blog titles

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,5 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { getBlogs } from '../services/blogs';
 
 export default function Main() {
   const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      const resp = await getBlogs();
+      setBlogs(resp);
+    };
+    fetchBlogs();
+  }, []);
+
+  return (
+    <main>
+      {blogs.map((blog) => (
+        // <BlogCard key={blog.id} {...blog} />
+        <p key={blog.id}>{blog.title}</p>
+      ))}
+    </main>
+  );
 }
