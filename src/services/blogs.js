830df440064d5b698c739c93252120546701818a
Alex Orlet <AlexOrlet89@gmail.com>
2022-03-15 17:34:24 -0700

    comment added

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,6 +1,7 @@
 import { checkError, client } from './client';
 
 export async function getBlogs() {
+  //comment
   const resp = await client.from('blogs').select('*');
   return checkError(resp);
 }
